b571449dec6e8ba6344aedc5261efd49b75d7ade
liukan <liukan@bytedance.com>
2017-08-23 00:08:48 +0800

    update favicon
    Change-Id: I4676a8e214a6c6456772cb91592d55122399b8d5

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ssh root@47.74.129.237 <<START
+cd /root/worksapce/nodercms
+sh run.sh
+START
---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cat PID | xargs kill
+git pull
+npm install || true
+nohup npm start 2>&1 >> run.log &
